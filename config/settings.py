7a9da5d4c23fc32938b14c4bc056abefc5d8e67e
Taufia Hussain <taufia.hussain@gmail.com>
2025-11-22 22:46:21 +0100

    Allow all hosts for Render demo

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
